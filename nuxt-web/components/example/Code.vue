51d3445f3596a5c718052703bcc4d3b983560739
Jeremy Butler <jeremy@jeremymbutler.com>
2026-01-12 13:59:23 +1000

    Fix copy

--- nuxt-web/components/example/Code.vue
+++ nuxt-web/components/example/Code.vue
@@ -1,31 +1,20 @@
 <template>
-  <div class="flex items-center justify-between gap-6 overflow-hidden bg-white px-5 py-4 max-md:flex-col dark:bg-white/[3%]">
+  <div
+    class="flex items-center justify-between gap-6 overflow-hidden bg-white px-5 py-4 max-md:flex-col dark:bg-white/[3%]">
     <div class="flex items-center gap-3 max-md:text-center">
-      <div class="flex size-5 shrink-0 items-center justify-center rounded-full bg-indigo-50 max-md:hidden dark:bg-white/5">
-        <Icon
-          name="ph:code-bold"
-          class="w-3 shrink-0 text-indigo-600 dark:text-white/50" />
-      </div>
       <div
-        class="text-pretty font-mono text-sm text-gray-500 dark:text-white/50">
+        class="flex size-5 shrink-0 items-center justify-center rounded-full bg-indigo-50 max-md:hidden dark:bg-white/5">
+        <Icon name="ph:code-bold" class="w-3 shrink-0 text-indigo-600 dark:text-white/50" />
+      </div>
+      <div class="text-pretty font-mono text-sm text-gray-500 dark:text-white/50">
         {{ code }}
       </div>
     </div>
-    <Button
-      color="secondary"
-      size="sm"
-      class="max-md:h-12 max-md:w-full md:w-24"
+    <Button color="secondary" size="sm" class="max-md:h-12 max-md:w-full md:w-24"
       @click="copyToClipboard(props.code), copied()">
-      <Icon
-        v-if="showCopied"
-        name="🎉"
-        class="size-3" />
-      <Icon
-        v-if="!showCopied"
-        name="heroicons:square-2-stack"
-        class="size-3" />
+      <Icon v-if="!showCopied" name="heroicons:square-2-stack" class="size-3" />
       <span class="text-sm/none">
-        <span v-if="showCopied">Copied</span>
+        <span v-if="showCopied">Copied!</span>
         <span v-else>Copy</span>
       </span>
     </Button>
@@ -56,6 +45,6 @@ function copied() {
 
 <style scoped>
 :deep(.option) {
-    @apply text-base underline decoration-indigo-200 dark:decoration-white/10 underline-offset-[6px] decoration-dashed text-indigo-500 dark:text-white inline-flex;
+  @apply text-base underline decoration-indigo-200 dark:decoration-white/10 underline-offset-[6px] decoration-dashed text-indigo-500 dark:text-white inline-flex;
 }
 </style>
